6ebf7433a197498e5f547c8cac40ce6225666ff7
cazdlt <cazdlt@gmail.com>
2020-08-25 11:18:23 -0500

    tests

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="isimws", packages=find_packages())
---
+++ tests/test_va.py
@@ -0,0 +1,62 @@
+from isimws.va.configure import update_property
+from isimws.va.auth import Session
+import pytest
+
+@pytest.fixture
+def sesion():
+    env="int"
+    u=****@local"
+    p=****=="qa" else "Colpensiones.2019"
+    url="10.120.70.181" if env=="qa" else "10.130.70.142"
+
+    s=Session(u,p,url,None)
+    return s
+
+def test_list_property_files(sesion):    
+    out=update_property.list_files(sesion)
+    assert isinstance(out,list)
+    assert len(out)>0
+
+def test_get_property_value(sesion):    
+    property_file="scriptframework.properties"
+    not_found=update_property.get_property_value(sesion,property_file,"XX")
+    assert not_found["result"]=="property_not_found"
+
+    one=update_property.get_property_value(sesion,property_file,"ITIM.java.access.util")
+    assert one["PropertyName"]=="ITIM.java.access.util"
+
+    multi=update_property.get_property_value(sesion,property_file)
+    assert isinstance(multi,list)
+    assert len(multi)>0
+
+def test_add_property(sesion):
+    property_file="CustomLabels.properties"
+    property_name="test"
+    property_value="test"
+    r=update_property.add_property(sesion,property_file,property_name,property_value)
+    assert r["result"]=="property_already_exist"
+
+
+def test_update_property(sesion):
+    property_file="CustomLabels.properties"
+    property_name="test"
+    property_value="test"
+
+    old_val=update_property.get_property_value(sesion,property_file,property_name)["PropertyValue"]
+    if property_value==old_val:
+        property_value+="X"
+
+    r=update_property.update_property(sesion,property_file,property_name,property_value)
+    assert r=="OK"
+
+    new_val=update_property.get_property_value(sesion,property_file,property_name)["PropertyValue"]
+    assert new_val==property_value
+
+    r=update_property.update_property(sesion,property_file,property_name,old_val)
+    assert r=="OK"
+
+def test_delete_property(sesion):
+    property_file="CustomLabels.properties"
+    property_name="test"
+    r=update_property.delete_property(sesion,property_file,property_name)
+    print(r)
