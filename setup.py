6e643212eb4012b9bfad159c0d1438c3119d3378
cazdlt <cazdlt@gmail.com>
2020-09-25 11:34:17 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ URL = 'https://github.com/cazdlt/pyisim'
 EMAIL = 'cazdlt@gmail.com'
 AUTHOR = 'Andrés Zamora'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = "" #Get the version from the package __init__.py
+VERSION = "0.1.0" # Get the version from the package __init__.py
 REQUIRED = [
     "requests >= 2.23.0",
     "zeep >= 3.4.0"
 ]
-EXTRAS=[]
+EXTRAS={}
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -33,57 +33,10 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -91,13 +44,31 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
+    download_url=f'{URL}/archive/v{VERSION}.tar.gz',
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*","pycolp"]),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
+    keywords = ['isim', 'ibm-security', 'iam'],
     license='MIT',
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.8',
+    ]
+
 )
