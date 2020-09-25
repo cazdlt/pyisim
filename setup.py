fc969e096613625914594ee9a1d201de69f9cf26
cazdlt <cazdlt@gmail.com>
2020-09-25 10:17:25 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup,find_packages
 
-setup(name="isimws", packages=find_packages())
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+DESCRIPTION="Easy to use Python client for IBM Security Identity Manager (ISIM/ITIM) web services (SOAP and REST APIs) "
+
+setup(
+  name="pyisim",
+  version="0.0.1",
+  description=DESCRIPTION,
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author="Camilo Andrés Zamora",
+  author_email="cazdlt@gmail.com",
+  license="MIT",
+  packages=find_packages(),
+  zip_safe=False
+)
