257397c6b5302428fb286e095e937660fbbaa79f
cazdlt <cazdlt@gmail.com>
2020-10-16 16:12:59 -0500

    rtd ahora si

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
@@ -62,3 +64,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+master_doc = 'index'
